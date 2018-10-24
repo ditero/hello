f98b8c03011a9e468e4f9fb9b4cf2edd272b4f63
ditero <anelesigenu@gmail.com>
2018-10-24 15:14:06 +0200

    Updates

---
+++ precache-manifest.3f35ba1680e8ca59f156b9d6b7eb50dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "fb0de5af4ecba5ba04e4",
+    "url": "/hello/static/js/main.fb0de5af.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/hello/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "fb0de5af4ecba5ba04e4",
+    "url": "/hello/static/css/main.7de6d70f.chunk.css"
+  },
+  {
+    "revision": "67016150abb071d52c3eeb0a80747e6f",
+    "url": "/hello/index.html"
+  }
+];
